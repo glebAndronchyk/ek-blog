7da0533ab209ac358c86bb16f20e50242a452025
Gleb <gleb.andronchyk@gmail.com>
2023-01-11 14:59:15 +0200

    added-app-component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from "./Components/app/App";
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="text-blue">dsadsasadsa</div>
+    <App />
   </React.StrictMode>
 );
