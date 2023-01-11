47ceebcdd0ad5271b657265c91e220e0fd1478a5
Gleb <gleb.andronchyk@gmail.com>
2023-01-11 14:57:43 +0200

    installed-react-router

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className="text-blue">dsadsasadsa</div>
   </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+
+    },
     extend: {},
   },
   plugins: [],
