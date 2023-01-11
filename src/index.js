e5e2f3151ab925a5e1d94e1e4408f5029205b3a4
Gleb <gleb.andronchyk@gmail.com>
2023-01-11 16:46:15 +0200

    structurized project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from "./Components/app/App";
 
-import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
   ],
   theme: {
     colors: {
-
+      'app-red': '#DD424C',
+      'app-black': {
+        100: '#353535',
+        200: '#171717'
+      },
+      'gray': {
+        100: '#C6C6C6',
+        200: '#EAEAEA',
+        300: '#8C8C8C',
+        400: '#B0B0B0'
+      },
     },
     extend: {},
   },
