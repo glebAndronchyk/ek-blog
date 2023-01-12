765151646007cb85affc7121f871730481cb824c
Gleb <gleb.andronchyk@gmail.com>
2023-01-12 12:16:22 +0200

    setuped tailwind config(theme)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     './public/index.html',
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    colors: {
-      'app-red': '#DD424C',
-      'app-black': {
-        100: '#353535',
-        200: '#171717'
+    extend: {
+      colors: {
+        'app-red': '#DD424C',
+        'black': '#1D2939',
+        'blue': {
+          100: '#253661'
+        },
+        'gray': {
+          100: '#C6C6C6',
+          200: '#EAEAEA',
+          300: '#8C8C8C',
+          400: '#B0B0B0',
+          500: '#667085'
+        },
       },
-      'gray': {
-        100: '#C6C6C6',
-        200: '#EAEAEA',
-        300: '#8C8C8C',
-        400: '#B0B0B0'
+      fontFamily: {
+        code: ['Code7x5', 'sans-serif'],
+        poppins: ['Poppins', "sans-serif"],
+        lato: ['Lato', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
     },
-    extend: {},
   },
   plugins: [],
 }
