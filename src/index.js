c8b027c75b0041addcbb494ca2c2c426970bffc4
Gleb <gleb.andronchyk@gmail.com>
2023-01-12 10:21:14 +0200

    index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import App from "./Components/app/App";
 
-import './styles/index.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
