be4924d36d10c9d4ef5661b0b8f3e1d2252a0c6e
Gleb <gleb.andronchyk@gmail.com>
2023-01-12 10:44:57 +0200

    removed test from index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./components/app/App";
 
 import './index.css';
-const test = 'test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
