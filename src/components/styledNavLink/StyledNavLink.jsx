2a137bc88c27e937527b767a2ce0fedab00a149b
Gleb <gleb.andronchyk@gmail.com>
2023-01-13 16:58:47 +0200

    reworked StyledNavLink with classnames library

--- src/components/styledNavLink/StyledNavLink.jsx
+++ src/components/styledNavLink/StyledNavLink.jsx
@@ -1,9 +1,18 @@
 import {NavLink} from "react-router-dom";
+import classNames from "classnames";
 
 const StyledNavLink = () => {
 
+  const currentClassName = (checker) => {
+    const className = classNames({
+      'nav-link': !checker,
+      'nav-link__active': checker
+    });
+    return className;
+  }
+
   return (
-    <NavLink to='/posts' className={({isActive}) => isActive ? 'nav-link__active' : 'nav-link'}>Hello</NavLink>
+    <NavLink to='/posts' className={({isActive}) => currentClassName(isActive)}>Hello</NavLink>
   )
 }
 
