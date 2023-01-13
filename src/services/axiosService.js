ae9ba19e445f0e57e560abf3db8a09c68a912357
Gleb <gleb.andronchyk@gmail.com>
2023-01-13 09:30:48 +0200

    deleted config directory(no need). Reworked axiosService.js(added axios.interceptors in it)

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -1,5 +1,10 @@
+import axios from "axios";
+
 import getToken from "../helpers/getToken";
-import axiosInstance from "../config/axios/axiosInstance";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE,
+});
 
 axiosInstance.interceptors.request.use((config) => {
     const token = getToken();
@@ -19,3 +24,5 @@ axiosInstance.interceptors.request.use((config) => {
 axiosInstance.interceptors.response.use((config) => {
   //TODO: makeAnErrorHandler
 });
+
+export default axiosInstance;
