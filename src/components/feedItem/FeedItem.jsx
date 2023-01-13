11284fe1d5220f601d62fd5f1cd82af5f1907383
Gleb <gleb.andronchyk@gmail.com>
2023-01-13 13:44:50 +0200

    responsively styled FeedItem component

--- src/components/feedItem/FeedItem.jsx
+++ src/components/feedItem/FeedItem.jsx
@@ -2,13 +2,13 @@ import FeedItemPlug from '../../assets/images/FeedItemPlug.png'
 
 const FeedItem = () => {
   return (
-    <div className='flex flex-row w-4/6 justify-between p-6 rounded-3xl'>
-      <div className='flex flex-col pl-1.5 py-4 max-w-[610px]'>
-        <span className='font-graphik-400 text-gray-500 uppercase text-xs mb-7'>Creation Data</span>
-        <h3 className='font-graphik font-semibold text-black text-2xl mb-2.5'>10 Hilarious Cartoons That Depict Real-Life Problems</h3>
-        <p className='text-lg font-light text-gray-600'>Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks...</p>
+    <div className='flex flex-row px-6 items-center justify-between border-2 md:w-4/6 md:rounded-3xl lg:p-4'>
+      <div className='flex flex-col pl-1.5 py-4 max-w-[610px] 2xl:max-w-full'>
+        <span className='font-graphik-400 text-gray-500 uppercase text-xs mb-7 2xl:text-base'>Creation Data</span>
+        <h3 className='font-graphik font-semibold text-black text-2xl mb-2.5 2xl:text-3xl'>10 Hilarious Cartoons That Depict Real-Life Problems</h3>
+        <p className='text-lg font-light text-gray-600 2xl:text-lg'>Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks...</p>
       </div>
-      <img src={FeedItemPlug} alt="item-image"/>
+      <img className='hidden lg:block lg:pl-1' src={FeedItemPlug} alt="item-image"/>
     </div>
   );
 }
