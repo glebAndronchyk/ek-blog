a025d1047bda326ac4c61feb3ffe9aa260ff5d9b
Gleb <gleb.andronchyk@gmail.com>
2023-01-16 15:43:02 +0200

    added StyledNavLink to Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,41 @@
 import {Link} from "react-router-dom";
+import StyledNavLink from "../styledNavLink/StyledNavLink";
+import Button from "../button/Button";
 
-import Logo from '../../assets/images/logo.png'
+import Logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
     <header className='px-16 py-4 flex justify-between items-center bg-blue-100 shadow-lg shadow-blue-100'>
       <Link to='/'>
         <h1>
-          <img className='h-8' src={Logo} alt="ekreative"/>
+          <img
+            className='h-8'
+            src={Logo}
+            alt="ekreative"
+          />
         </h1>
       </Link>
       <div className='flex justify-between'>
         <ul className='flex justify-between '>
           <li className='mr-7'>
-            {/*<StyledNavLink to='/posts' type='header'>Posts</StyledNavLink>*/}
+            <StyledNavLink
+              to='/posts'
+              type='header'
+            >
+              Posts
+            </StyledNavLink>
           </li>
           <li className='mr-7'>
-            {/*<StyledNavLink to='/announcements' type='header'>Announcements</StyledNavLink>*/}
+            <StyledNavLink
+              to='/announcements'
+              type='header'
+            >
+              Announcements
+            </StyledNavLink>
           </li>
         </ul>
-        {/*<Button className='ml-7' type='header-btn' disabled='false'>dsadsadsa</Button>*/}
+        <Button className='ml-7' type='header-btn'>Login</Button>
       </div>
     </header>
   );
