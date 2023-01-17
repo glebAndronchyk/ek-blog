b18da1064a551c3dd3039c68a44823535fafc045
Gleb <gleb.andronchyk@gmail.com>
2023-01-17 13:55:19 +0200

    added new color to section 'Blue'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    './public/index.html',
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         'app-red': '#D03450',
-        'black': '#1D2939',
-        'blue': {
-          100: '#253661'
+        black: '#1D2939',
+        blue: {
+          100: '#253661',
+          200: '#475467',
         },
-        'gray': {
+        gray: {
           100: '#C6C6C6',
           200: '#EAEAEA',
           300: '#8C8C8C',
           400: '#B0B0B0',
-          500: '#667085'
+          500: '#667085',
         },
       },
       fontFamily: {
@@ -26,7 +24,7 @@ module.exports = {
         graphik: ['Graphik', 'sans-serif'],
         lato: ['Lato', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [
