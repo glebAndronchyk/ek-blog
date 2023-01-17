ca4872128f057edd22e9f9aaa9efbf9e535fff38
Gleb <gleb.andronchyk@gmail.com>
2023-01-17 14:02:11 +0200

    fully made banner for news pages

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,12 @@
-const Banner = () => {
+const Banner = props => {
+  const { label, children } = props;
+
   return (
-    <section className="bg-white flex items-center justify-center">
-      <div className="flex flex-col max-w-[640px] w-full items-center pt-28 pb-20">1</div>
+    <section className="relative z-0 bg-white flex items-center justify-center">
+      <div className="flex flex-col max-w-[640px] w-full items-center pt-28 pb-20">
+        <h2 className="text-7xl font-[600] mb-5 text-black">{label}</h2>
+        <p className="text-blue-200 text-2xl">{children}</p>
+      </div>
     </section>
   );
 };
