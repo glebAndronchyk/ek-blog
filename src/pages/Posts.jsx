eefde8035cd70f346549440cbdf47d86403322f8
Gleb <gleb.andronchyk@gmail.com>
2023-01-17 14:59:27 +0200

    Posts page base

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,12 @@
 import Banner from '../components/banner/Banner';
+import PostsContentSection from '../components/postsContentSection/PostsContentSection';
 
 const Posts = () => {
   return (
-    <Banner />
+    <>
+      <Banner label="Posts">Read them, or add them</Banner>
+      <PostsContentSection />
+    </>
   );
 };
 export default Posts;
