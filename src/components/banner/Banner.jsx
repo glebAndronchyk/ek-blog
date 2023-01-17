5a8de3c6d71bae12d0b9894b39609a99ac4813da
Gleb <gleb.andronchyk@gmail.com>
2023-01-17 14:29:18 +0200

    created banner.css with background style for Banner componnent

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,11 @@
+import './banner.css';
+
 const Banner = props => {
   const { label, children } = props;
 
   return (
-    <section className="relative z-0 bg-white flex items-center justify-center">
-      <div className="flex flex-col max-w-[640px] w-full items-center pt-28 pb-20">
+    <section className="relative posts-banner-background z-0 flex items-center justify-center">
+      <div className="flex flex-col max-w-[640px] w-full items-center pt-24 pb-16">
         <h2 className="text-7xl font-[600] mb-5 text-black">{label}</h2>
         <p className="text-blue-200 text-2xl">{children}</p>
       </div>
