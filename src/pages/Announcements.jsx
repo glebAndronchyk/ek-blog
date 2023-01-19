286d15a038ad8b7e3fd7eaedf80b3de17a4f296e
Gleb <gleb.andronchyk@gmail.com>
2023-01-19 14:47:03 +0200

    created custom bg functionallity

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -1,10 +1,15 @@
 import Banner from '../components/banner/Banner';
 import AnnouncementsList from '../components/announcementsList/AnnouncementsList';
 
+import AnnouncementsBg from '../assets/images/announcements-banner-bg.webp';
+
 const Announcements = () => {
   return (
     <>
-      <Banner label="Announcements">
+      <Banner
+        label="Announcements"
+        image={AnnouncementsBg}
+      >
         <span>Important news</span>
       </Banner>
       <AnnouncementsList />
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,15 @@
 import Banner from '../components/banner/Banner';
 import PostsList from '../components/postsList/PostsList';
 
+import PostsBg from '../assets/images/posts-banner-bg.jpg';
+
 const Posts = () => {
   return (
     <>
-      <Banner label="Posts">
+      <Banner
+        label="Posts"
+        image={PostsBg}
+      >
         <span>Read them, or add them</span>
       </Banner>
       <PostsList />
