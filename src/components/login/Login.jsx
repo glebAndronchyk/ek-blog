8c54335af5748e79b157034c235d27ad4cd6c90d
Gleb <gleb.andronchyk@gmail.com>
2023-01-20 15:52:57 +0200

    added key pressing logic

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,46 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
 import classNames from 'classnames';
 
 import LoginForm from '../loginForm/LoginForm';
+import { modalOpenStateChanged } from '../../slices/loginSlice';
 
 const Login = () => {
   const { isShown } = useSelector(state => state.login);
   const className = classNames({ hidden: !isShown });
+  const backgroundRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const handleBackgroundClick = event => {
+    if (event.target === backgroundRef.current) {
+      return dispatch(modalOpenStateChanged());
+    }
+    return null;
+  };
+
+  const handleEscPress = event => {
+    const { key } = event;
+    if (key === 'Escape') {
+      return dispatch(modalOpenStateChanged());
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    const { current } = backgroundRef;
+    current.addEventListener('click', handleBackgroundClick);
+    window.addEventListener('keydown', handleEscPress);
+    return () => {
+      current.removeEventListener('click', handleBackgroundClick);
+      window.removeEventListener('keydown', handleEscPress);
+    };
+  }, []);
 
   return (
-    <div className={className}>
+    <div
+      ref={backgroundRef}
+      className={`${className} fixed z-50 inset-0 flex justify-center items-center bg-black/[.5]`}
+    >
       <LoginForm />
     </div>
   );
