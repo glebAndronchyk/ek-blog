0011653d774e4d14aa60679ea04c959a27eb8465
Gleb <gleb.andronchyk@gmail.com>
2023-01-23 14:57:44 +0200

    imported preflight.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app/App';
 
+import './preflight.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
