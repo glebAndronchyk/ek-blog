707dd9abfa2d11145c476b616880c7d3c69b1d11
Gleb <gleb.andronchyk@gmail.com>
2023-01-23 14:58:05 +0200

    added user reducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import posts from '../slices/postsListSlice';
 import announcements from '../slices/announcementsListSlice';
 import login from '../slices/loginSlice';
+import user from '../slices/userSlice';
 
 const store = configureStore({
-  reducer: { posts, announcements, login },
+  reducer: { posts, announcements, login, user },
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
   devTools: process.env.NODE_ENC !== 'production',
 });
