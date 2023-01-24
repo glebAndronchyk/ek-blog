3af9e3accf9e46029312a4351b4df2cc830ec068
Gleb <gleb.andronchyk@gmail.com>
2023-01-24 17:16:40 +0200

    small refactoring

--- src/components/loginFormButton/LoginFormButton.jsx
+++ src/components/loginFormButton/LoginFormButton.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux';
 
 import Button from '../button/Button';
-import { IDLE, REJECTED } from '../../helpers/loadingStatus';
+import { LOADING } from '../../helpers/loadingStatus';
 import ButtonSpinner from '../buttonSpinner/ButtonSpinner';
 
 const LoginFormButton = () => {
   const { loading } = useSelector(state => state.user);
 
-  const loadingCondition = loading !== IDLE && loading !== REJECTED;
+  const loadingCondition = loading === LOADING;
   const insideContent = loadingCondition ? <ButtonSpinner /> : <span>Login</span>;
 
   return (
