7a6ac196184c7487a23763703ee563d42c4235f0
Gleb <gleb.andronchyk@gmail.com>
2023-01-24 13:51:07 +0200

    reworked with try catch

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,8 +2,12 @@ import axiosInstance from './axiosService';
 
 export const login = async data => {
   const { email, password } = data;
-  const response = await axiosInstance.post('/login', { email, password });
-  return response.data;
+  try {
+    const response = await axiosInstance.post('/login', { email, password });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.status);
+  }
 };
 
 export const register = () => {
