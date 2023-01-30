2d43d1f30969966296362b0163d71f4a140bc5d9
Gleb <gleb.andronchyk@gmail.com>
2023-01-30 09:49:24 +0200

    made a countdownTimer

--- src/features/ui/successWindow/SuccessWindow.jsx
+++ src/features/ui/successWindow/SuccessWindow.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const SuccessWindow = () => {
+  const [timeLeft, setTimeLeft] = useState(5);
+  const routerNavigate = useNavigate();
+
+  useEffect(() => {
+    const countdownTimer = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    if (timeLeft === 0) {
+      clearInterval(countdownTimer);
+      routerNavigate('/');
+    }
+
+    return () => {
+      clearInterval(countdownTimer);
+    };
+  }, [timeLeft]);
+
   return (
-    <div>
-      <span>SUCCESS</span>
-    </div>
+    <>
+      <p className="text-center">You have been successfully registered</p>
+      <p className="text-center">You will be redirected in {timeLeft} seconds</p>
+    </>
   );
 };
 
