4a36c7b784e81ddc4187ffd5b4ff95dbc48e0776
Gleb <gleb.andronchyk@gmail.com>
2023-01-30 11:25:27 +0200

    textInputs refactroing

--- src/features/login/loginForm/LoginForm.jsx
+++ src/features/login/loginForm/LoginForm.jsx
@@ -1,16 +1,15 @@
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import { tryToLogin } from 'redux/slices/userSlice';
 import { modalClosed } from 'redux/slices/modalSlice';
 import { loginInputs } from 'helpers/inputsData';
 import ErrorPlug from 'features/ui/errorPlug/ErrorPlug';
 import Form from 'features/ui/form/Form';
-import FormInput from 'features/ui/formInput/FormInput';
 import AuthButton from 'features/ui/authButton/AuthButton';
 import StyledNavLink from 'features/ui/styledNavLink/StyledNavLink';
+import TextInputs from 'features/registration/textInputs/TextInputs';
 
 const LoginForm = () => {
   const {
@@ -35,21 +34,6 @@ const LoginForm = () => {
 
   if (status && status !== 400) return ErrorPlug;
 
-  const inputs = Object.keys(loginInputs).map(item => {
-    const { className, placeholder, type, label, options } = loginInputs[item];
-    return (
-      <FormInput
-        key={uuidv4()}
-        register={register}
-        errors={errors}
-        className={className}
-        placeholder={placeholder}
-        type={type}
-        label={label}
-        options={options}
-      />
-    );
-  });
   return (
     <Form
       onSubmit={handleSubmit(onSubmit)}
@@ -57,7 +41,11 @@ const LoginForm = () => {
     >
       <h3 className="font-code text-3xl text-black mb-3">Hello World!</h3>
       <span className="font-lato font-[600] text-gray-300 text-base mb-6">Login with your details</span>
-      {inputs}
+      <TextInputs
+        register={register}
+        errors={errors}
+        castObject={loginInputs}
+      />
       <AuthButton
         className="mb-2"
         label="Login"
