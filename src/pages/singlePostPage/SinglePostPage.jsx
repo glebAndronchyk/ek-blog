3ebc249b8c7c5f8881ae03ee8e250cbe15a9605f
Gleb <gleb.andronchyk@gmail.com>
2023-02-03 15:36:54 +0200

    comments item

--- src/pages/singlePostPage/SinglePostPage.jsx
+++ src/pages/singlePostPage/SinglePostPage.jsx
@@ -6,6 +6,7 @@ import getDateInCorrectFormat from 'helpers/getDateInCorrectFormat';
 import { LOADING, REJECTED } from 'helpers/loadingStatus';
 import Spinner from 'features/ui/spinner/Spinner';
 import ErrorPlug from 'features/ui/errorPlug/ErrorPlug';
+import Comments from 'features/postComments/comments/Comments';
 import useGetPost from 'hooks/useGetPost';
 
 const SinglePostPage = () => {
@@ -25,13 +26,16 @@ const SinglePostPage = () => {
     // TODO: RETURN BUTTON
     // TODO: REMOVE EDIT BUTTONS
     <article className="w-8/12 mx-auto pt-4">
-      <h2 className="font-[600] text-black text-3xl relative mb-2 single-post-deco before:bg-app-red">{title}</h2>
-      <span className="block pl-4 mb-2 font-[400] text-gray-500 text-base">
-        By: {`${firstname} ${lastname}. `}
-        {`Created at: ${getDateInCorrectFormat(createdAt)}, Updated at: ${getDateInCorrectFormat(updatedAt)}`}
-      </span>
-      <p className="relative text-lg single-post-deco before:bg-blue-100 break-words">{body}</p>
-      {/*TODO: MAKE COMMENTS SECTION*/}
+      <div className="article-content">
+        <h2 className="font-[600] text-black text-3xl relative mb-2 single-post-deco before:bg-app-red">{title}</h2>
+        <span className="block pl-4 mb-2 font-[400] text-gray-500 text-base">
+          By: {`${firstname} ${lastname}. `}
+          {`Created at: ${getDateInCorrectFormat(createdAt)}, Updated at: ${getDateInCorrectFormat(updatedAt)}`}
+        </span>
+        <p className="relative text-lg single-post-deco before:bg-blue-100 break-words">{body}</p>
+      </div>
+      <div className="border-t-2 w-full mb-2" />
+      <Comments />
     </article>
   );
 };
