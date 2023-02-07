278f9672faa5d71474d7f352efbe93bc4774fec8
Gleb <gleb.andronchyk@gmail.com>
2023-02-07 09:43:35 +0200

    cleaning

--- src/services/commentsService.js
+++ src/services/commentsService.js
@@ -1,11 +1,5 @@
 import axiosInstance from './axiosService';
 
-export const getItem = async (entity, id) => {
-  const params = { _expand: 'user' };
-  const response = await axiosInstance.get(`/${entity}/${id}`, { params });
-  return response.data;
-};
-
 export const getPostRelatedComments = async (postId, pageNumber = 1) => {
   const params = { _page: pageNumber, _sort: 'createdAt', _order: 'desc', _limit: 10, _expand: 'user' };
   const response = await axiosInstance.get(`posts/${postId}/comments`, { params });
@@ -24,15 +18,6 @@ export const createComment = async (data) => {
 export const editNews = async (entity, data, id) => {
   try {
     const response = await axiosInstance.patch(`/${entity}/${id}`, { ...data });
-    return response.data;
-  } catch (error) {
-    throw new Error(JSON.stringify(error.response));
-  }
-};
-
-export const deleteNews = async entity => {
-  try {
-    const response = await axiosInstance.delete(`/${entity}`);
     return response.data;
   } catch (error) {
     throw new Error(JSON.stringify(error.response));
