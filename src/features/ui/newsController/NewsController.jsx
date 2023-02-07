283f13679c21788f69206503e6bbf402f91c5113
Gleb <gleb.andronchyk@gmail.com>
2023-02-07 09:45:01 +0200

    prop types changes

--- src/features/ui/newsController/NewsController.jsx
+++ src/features/ui/newsController/NewsController.jsx
@@ -4,24 +4,24 @@ import EditItem from 'features/newsControl/editItem/EditItem';
 import DeleteItem from 'features/newsControl/deleteItem/DeleteItem';
 
 const NewsController = props => {
-  const { configuration } = props;
+  const { modalConfiguration } = props;
 
   return (
     <div className="flex justify-evenly absolute z-50 right-0 top-0 max-w-[100px] w-full pt-2">
-      <EditItem configuration={configuration} />
-      <DeleteItem configuration={configuration} />
+      <EditItem modalConfiguration={modalConfiguration} />
+      <DeleteItem modalConfiguration={modalConfiguration} />
     </div>
   );
 };
 
 NewsController.propTypes = {
-  configuration: PropTypes.exact({
-    id: PropTypes.number.isRequired,
+  modalConfiguration: PropTypes.exact({
+    id: PropTypes.number,
     entity: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
+    createdAt: PropTypes.string,
+    title: PropTypes.string,
+    body: PropTypes.string,
+    name: PropTypes.string,
   }).isRequired,
 };
 
