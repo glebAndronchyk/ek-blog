8dad77cacc8fe5d39587b73635e58af41023c7d1
Gleb <gleb.andronchyk@gmail.com>
2023-02-08 14:48:55 +0200

    user profile menu

---
+++ src/features/ui/userProfileMenu/UserProfileMenu.jsx
@@ -0,0 +1,10 @@
+const UserProfileMenu = () => {
+  return (
+    <ul className="flex items-center justify-evenly bg-app-red p-2 border-t-2">
+      <li>Settings</li>
+      <li>Your news(in development)</li>
+    </ul>
+  );
+};
+
+export default UserProfileMenu;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,9 +1,15 @@
 import Banner from 'features/ui/banner/Banner';
+import UserProfileMenu from 'features/ui/userProfileMenu/UserProfileMenu';
 
 import userProfileBannerImage from 'assets/images/registrationBg.png';
 
 const UserProfile = () => {
-  return <Banner image={userProfileBannerImage} />;
+  return (
+    <>
+      <Banner image={userProfileBannerImage} />
+      <UserProfileMenu />
+    </>
+  );
 };
 
 export default UserProfile;
