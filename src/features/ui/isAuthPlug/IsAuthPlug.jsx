5a9feadee586af343311f375d4d375fb58fb0097
Gleb <gleb.andronchyk@gmail.com>
2023-02-08 09:22:07 +0200

    height changes

--- src/features/ui/isAuthPlug/IsAuthPlug.jsx
+++ src/features/ui/isAuthPlug/IsAuthPlug.jsx
@@ -1,6 +1,6 @@
 const IsAuthPlug = () => {
   return (
-    <div className="rounded-[20px] border-2 flex justify-center items-center h-[72px] mb-2 bg-gray-50">
+    <div className="rounded-[20px] border-2 flex justify-center items-center h-[56px] mb-2 bg-gray-50">
       Login in order to make comments
     </div>
   );
