db116680afbc2921723f465903058c53aa1ea8c2
Gleb <gleb.andronchyk@gmail.com>
2023-02-09 12:16:20 +0200

    updating local data while editing profile data

--- src/features/ui/banner/insideContent/BannerUserProfileContent.jsx
+++ src/features/ui/banner/insideContent/BannerUserProfileContent.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import { getUserDataFromStorage } from 'helpers/localStorage';
 
 const BannerUserProfileContent = () => {
-  const { firstname, lastname, avatar } = getUserDataFromStorage();
+  const [userFirstName, setUserFirstName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+  const { dataChanged } = useSelector(state => state.user);
+
+  useEffect(() => {
+    const { firstname, lastname, avatar } = getUserDataFromStorage();
+    setUserFirstName(firstname);
+    setUserLastName(lastname);
+    setUserAvatar(avatar);
+  }, [dataChanged]);
 
   return (
     <>
       <img
-        src={avatar}
+        src={userAvatar}
         alt="avatar"
         className="w-[120px] h-[120px] mb-2 rounded-full bg-gray-50 p-[2px]"
       />
-      <span className="text-lg text-gray-50">{`${firstname} ${lastname}(You)`}</span>
+      <span className="text-lg text-gray-50">{`${userFirstName} ${userLastName}(You)`}</span>
     </>
   );
 };
