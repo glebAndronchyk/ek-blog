183170f963726c91ee8fe980d2a0a90709d59aa1
Gleb <gleb.andronchyk@gmail.com>
2023-02-13 11:36:56 +0200

    reworked with AvatarImage.jsx

--- src/features/ui/banner/insideContent/BannerUserProfileContent.jsx
+++ src/features/ui/banner/insideContent/BannerUserProfileContent.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import AvatarImage from 'features/ui/avatarImage/AvatarImage';
 import { getUserDataFromStorage } from 'helpers/localStorage';
-import avatars from "helpers/avatars";
+import avatars from 'helpers/avatars';
 
 const BannerUserProfileContent = () => {
   const [userFirstName, setUserFirstName] = useState('');
@@ -19,10 +20,9 @@ const BannerUserProfileContent = () => {
 
   return (
     <>
-      <img
-        src={userAvatar || avatars[0]}
-        alt="avatar"
+      <AvatarImage
         className="w-[120px] h-[120px] mb-2 rounded-full bg-gray-50 p-[2px]"
+        src={userAvatar || avatars[0]}
       />
       <span className="text-lg text-gray-50">{`${userFirstName} ${userLastName}(You)`}</span>
     </>
