6316f790c405f65fad02d18f22af2101bae0cf62
Gleb <gleb.andronchyk@gmail.com>
2023-02-14 10:37:03 +0200

    length: 400 -> 250

--- src/helpers/processLongText.js
+++ src/helpers/processLongText.js
@@ -1,5 +1,5 @@
 const processLongText = text => {
-  return text.length > 400 ? `${text.slice(0, 400)}...` : text;
+  return text.length > 250 ? `${text.slice(0, 250)}...` : text;
 };
 
 export default processLongText;
