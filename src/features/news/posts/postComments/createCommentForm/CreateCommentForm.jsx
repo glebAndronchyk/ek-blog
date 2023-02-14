4cc07f284adaec11e2da104686f904f299aabd0e
Gleb <gleb.andronchyk@gmail.com>
2023-02-14 14:42:08 +0200

    single post page: breakpoints-320-1516 adaptive

--- src/features/news/posts/postComments/createCommentForm/CreateCommentForm.jsx
+++ src/features/news/posts/postComments/createCommentForm/CreateCommentForm.jsx
@@ -31,9 +31,12 @@ const CreateCommentForm = props => {
   });
   const dispatch = useDispatch();
   const { userActionLoading } = useSelector(state => state.comments);
-  const formWrapperClassName = classNames('w-full border-2 flex items-center rounded-[20px] mb-4 pl-4 box-border', {
-    'border-app-red': userActionLoading === REJECTED,
-  });
+  const formWrapperClassName = classNames(
+    'w-full border-2 flex items-center rounded-[20px] mb-4 pl-4 box-border bg-white',
+    {
+      'border-app-red': userActionLoading === REJECTED,
+    },
+  );
 
   useEffect(() => {
     if (isSubmitSuccessful && userActionLoading === IDLE) {
@@ -49,15 +52,19 @@ const CreateCommentForm = props => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={formWrapperClassName}>
         <TextareaAutosize
-          className="h-full max-h-[200px] max-w-[90%] w-full py-2 text-justify
-           overflow-x-hidden overflow-y-auto focus-visible:outline-0
-            resize-none border-0 focus:ring-0"
+          className="h-full max-h-[200px] max-w-[70%] w-full py-2 text-justify overflow-x-hidden overflow-y-auto focus-visible:outline-0 resize-none border-0 focus:ring-0
+                     md:max-w-[80%]
+                     2xl:max-w-[90%]"
           {...register('body')}
           disabled={userActionLoading === LOADING}
           maxRows={10}
           maxLength={600}
         />
-        <div className="flex justify-center items-center max-w-[10%] w-full flex-col border-l-2">
+        <div
+          className="flex justify-center items-center max-w-[30%] w-full flex-col border-l-2
+                     md:max-w-[20%]
+                     2xl:max-w-[10%]"
+        >
           {userActionLoading === LOADING ? (
             <Spinner
               wrapperClassName="pt-2"
