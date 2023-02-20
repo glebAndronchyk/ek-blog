c2be13f05b11faf0237aec2ae4145b2535886eb0
Gleb <gleb.andronchyk@gmail.com>
2023-02-20 06:39:44 +0200

    condition for long text with 2000 breakpoint

--- src/helpers/processLongText.js
+++ src/helpers/processLongText.js
@@ -1,5 +1,10 @@
-const processLongText = text => {
-  return text.length > 250 ? `${text.slice(0, 250)}...` : text;
+const processLongText = (text, breakpoint) => {
+  let breakpointValue = 130;
+  if (breakpoint > 2000) {
+    breakpointValue = 220;
+  }
+
+  return text.length > breakpointValue ? `${text.slice(0, breakpointValue)}...` : text;
 };
 
 export default processLongText;
