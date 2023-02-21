52b03050fe4c5d960ca2cdf12a9be7f80b8caa1f
Gleb <gleb.andronchyk@gmail.com>
2023-02-21 08:11:38 +0200

    reworked with StyledNavLinkGroup.jsx

--- src/features/ui/userProfileMenu/UserProfileMenu.jsx
+++ src/features/ui/userProfileMenu/UserProfileMenu.jsx
@@ -1,9 +1,15 @@
-import MenuContentItems from 'features/ui/userProfileMenu/MenuContentItems';
+import StyledNavLinkGroup from 'features/ui/styledNavLink/StyledNavLinkGroup';
 
 const UserProfileMenu = () => {
   return (
     <ul className="flex items-center justify-evenly bg-app-red p-2 border-t-2">
-      <MenuContentItems />
+      <StyledNavLinkGroup
+        contentTable={[
+          { label: 'Settings', path: 'settings' },
+          { label: 'Your posts', path: 'posts' },
+        ]}
+        className="text-black after:bg-black after:bottom-[-2px]"
+      />
     </ul>
   );
 };
