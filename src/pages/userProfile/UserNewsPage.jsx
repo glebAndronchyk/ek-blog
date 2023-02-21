1a1be4db58728b22d2546b589bb9857966690d4e
Gleb <gleb.andronchyk@gmail.com>
2023-02-21 10:58:32 +0200

    adiitional features for UserNewsPage.jsx (StyledNavLinkGroup in ul, breaker)

--- src/pages/userProfile/UserNewsPage.jsx
+++ src/pages/userProfile/UserNewsPage.jsx
@@ -5,13 +5,16 @@ import StyledNavLinkGroup from 'features/ui/styledNavLink/StyledNavLinkGroup';
 const UserNewsPage = () => {
   return (
     <>
-      <StyledNavLinkGroup
-        contentTable={[
-          { label: 'Posts', path: 'posts' },
-          { label: 'Announcements', path: 'announcements' },
-        ]}
-        className=""
-      />
+      <ul className="flex justify-around py-2 max-w-[500px] w-full">
+        <StyledNavLinkGroup
+          contentTable={[
+            { label: 'Posts', path: 'posts' },
+            { label: 'Announcements', path: 'announcements' },
+          ]}
+          className="text-black after:bg-black after:bottom-[-2px]"
+          breaker="/"
+        />
+      </ul>
       <Outlet />
     </>
   );
