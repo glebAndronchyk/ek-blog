7fab491b853490a70da2b69336c2edc9bacf9f52
Gleb <gleb.andronchyk@gmail.com>
2023-02-22 08:54:56 +0200

    removed data-testId

--- src/features/ui/isAuthPlug/IsAuthPlug.jsx
+++ src/features/ui/isAuthPlug/IsAuthPlug.jsx
@@ -1,9 +1,6 @@
 const IsAuthPlug = () => {
   return (
-    <div
-      className="rounded-[20px] border-2 flex justify-center items-center h-[56px] mb-2 bg-white"
-      data-testid="is-auth-plug"
-    >
+    <div className="rounded-[20px] border-2 flex justify-center items-center h-[56px] mb-2 bg-white">
       Login in order to make comments
     </div>
   );
