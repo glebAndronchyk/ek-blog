fed5b0279262b5e179e68d323f6f6da2ec4ad06c
Gleb <gleb.andronchyk@gmail.com>
2023-02-22 06:32:13 +0200

    IsAuthPlug.jsx test

--- src/features/ui/isAuthPlug/IsAuthPlug.jsx
+++ src/features/ui/isAuthPlug/IsAuthPlug.jsx
@@ -1,6 +1,9 @@
 const IsAuthPlug = () => {
   return (
-    <div className="rounded-[20px] border-2 flex justify-center items-center h-[56px] mb-2 bg-white">
+    <div
+      className="rounded-[20px] border-2 flex justify-center items-center h-[56px] mb-2 bg-white"
+      data-testid="is-auth-plug"
+    >
       Login in order to make comments
     </div>
   );
