98d93ae9f19f08d7a13688840e46502c1380e821
Gleb <gleb.andronchyk@gmail.com>
2023-02-23 06:50:16 +0200

    replaced element button with component Button.jsx

--- src/features/ui/buttons/closeButton/CloseButton.jsx
+++ src/features/ui/buttons/closeButton/CloseButton.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { modalClosed } from 'redux/slices/modalSlice';
+import Button from 'features/ui/buttons/button/Button';
 
 const CloseButton = () => {
   const dispatch = useDispatch();
@@ -11,14 +12,14 @@ const CloseButton = () => {
   };
 
   return (
-    <button
+    <Button
       onClick={closeModal}
       type="button"
       className="absolute top-2 right-2 rounded-full h-[40px] w-[40px] bg-app-red
                  md:hover:bg-[#DE8193] md:duration-300"
     >
       <FontAwesomeIcon icon="fa-solid fa-xmark" />
-    </button>
+    </Button>
   );
 };
 
